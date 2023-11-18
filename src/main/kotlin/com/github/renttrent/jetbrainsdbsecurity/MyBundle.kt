8bac43d9ee6a66ffcf9f4adc703d8abfdd9d091b
GitHub Action <action@github.com>
2023-11-18 09:42:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/renttrent/jetbrainsdbsecurity/MyBundle.kt
+++ src/main/kotlin/com/github/renttrent/jetbrainsdbsecurity/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.renttrent.jetbrainsdbsecurity
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/renttrent/jetbrainsdbsecurity/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/renttrent/jetbrainsdbsecurity/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.renttrent.jetbrainsdbsecurity.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
