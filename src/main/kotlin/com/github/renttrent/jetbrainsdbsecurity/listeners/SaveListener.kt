4c4439d46a94dc965771caaf4933a3fd1ba7653a
rei <reiballa@gmail.com>
2023-11-18 19:43:19 +0100

    fix junk

---
+++ src/main/kotlin/com/github/renttrent/jetbrainsdbsecurity/listeners/SaveListener.kt
@@ -0,0 +1,14 @@
+package com.github.renttrent.jetbrainsdbsecurity.listeners
+
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.fileEditor.FileDocumentManagerListener
+
+class SaveListener : FileDocumentManagerListener {
+    override fun beforeDocumentSaving(document: Document) {
+//   TODO
+    }
+
+    override fun beforeAllDocumentsSaving() {
+//        TODO
+    }
+}
